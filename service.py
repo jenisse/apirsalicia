aead2210097c12e9792a33c4f0f1a94507634501
Jenisse <jenisse@MacBook-Pro-de-Jenisse.local>
2018-04-23 08:05:10 -0500

    change .py

--- service.py
+++ service.py
@@ -11,7 +11,8 @@ def recommendations(dato):
     db_connect = create_engine('postgres://dhkuugcsguuvmc:72aae72895616c0a4afcb2fbc3ffdb9da9d5cf6a33373d3ed93b1490d32183b1@ec2-54-243-213-188.compute-1.amazonaws.com:5432/d1i8j5vuaca7m0')
     conn = db_connect.connect()
     rese_id ='id_investigador='+str(dato)
-    query=conn.execute( """select Similar_Orcid.title, Similar_Orcid.similarity_percentage, Publicacion_Alicia.url, Publicacion_Alicia.title as titulo_alicia from Similar_Orcid  INNER JOIN Publicacion_Alicia on Similar_Orcid.identifier=Publicacion_Alicia.identifier where link_dina like ('%' || ? || '%')""", (rese_id,))
+    #query=conn.execute( """select Similar_Orcid.title, Similar_Orcid.similarity_percentage, Publicacion_Alicia.url, Publicacion_Alicia.title as titulo_alicia from Similar_Orcid  INNER JOIN Publicacion_Alicia on Similar_Orcid.identifier=Publicacion_Alicia.identifier where link_dina like ('%' || ? || '%')""", (rese_id,))
+    query=conn.execute("select * from Investigador")
     result = {'recommendations': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
     return jsonify(result)
 
