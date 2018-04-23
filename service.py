dbdccce88a0bb2ee8d661df9905161620006b254
Jenisse <jenisse@MacBook-Pro-de-Jenisse.local>
2018-04-23 07:52:28 -0500

    procfile

--- service.py
+++ service.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask.ext.jsonpify import jsonify
 from sqlalchemy import create_engine
+import os
 
 app = Flask(__name__)
 
@@ -31,7 +32,8 @@ def shutdown():
     func()
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
