646198ab19664a93afc1f9e69291c5a306e0baf3
jenisse <jenisse.vereau@pucp.pe>
2018-07-01 10:24:20 -0500

    changes on querys because of db

--- service.py
+++ service.py
@@ -2,49 +2,60 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask.ext.jsonpify import jsonify
 from sqlalchemy import create_engine
-import os
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
-from flask import make_response
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
-app.config['DATABASE_URI'] = 'postgres://aaiekzlymkmulx:688099ba64af630040260c2d81a5354098b4773ca07fb0163d49ee36928fa342@ec2-50-16-196-238.compute-1.amazonaws.com:5432/de925n6fc4qpge'
 
-@app.route('/recomendaciones/<dato>', methods=['GET'])
-def recommendations(dato):
-    db_connect = create_engine('postgres://aaiekzlymkmulx:688099ba64af630040260c2d81a5354098b4773ca07fb0163d49ee36928fa342@ec2-50-16-196-238.compute-1.amazonaws.com:5432/de925n6fc4qpge')
-    conn = db_connect.connect()
-    rese_id ='id_investigador='+str(dato)
-    #query=conn.execute( """select so.title, so.similarity_percentage, pa.url, pa.title as titulo_alicia FROM "Similar_Orcid" so  INNER JOIN "Publicacion_Alicia" pa ON so.identifier=pa.identifier where so.link_dina like ('%' || ? || '%')""", (rese_id,))
-    stmt = text("""SELECT so.title, so.similarity_percentage, pa.url, pa.title as titulo_alicia FROM "Similar_Orcid" so  INNER JOIN "Publicacion_Alicia" pa ON so.identifier=pa.identifier where so.link_dina like ('%'|| :x)""")
-    query = conn.execute(stmt,x=rese_id)
-    #query=conn.execute('select * from "Investigador"')
-    result = {'recommendations': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-    return jsonify(result),{'Content-Type': 'application/json'}
 
-@app.route('/recomendacion_aleatoria/<dato>',methods=['GET']) 
-def randomRecommendation(dato):
-    db_connect = create_engine('postgres://aaiekzlymkmulx:688099ba64af630040260c2d81a5354098b4773ca07fb0163d49ee36928fa342@ec2-50-16-196-238.compute-1.amazonaws.com:5432/de925n6fc4qpge')
-    conn = db_connect.connect()
-    rese_id ='id_investigador='+str(dato)
-    #query=conn.execute( """select Similar_Orcid.title, Similar_Orcid.similarity_percentage, Publicacion_Alicia.url, Publicacion_Alicia.title as titulo_alicia from Similar_Orcid  INNER JOIN Publicacion_Alicia on Similar_Orcid.identifier=Publicacion_Alicia.identifier where link_dina like ('%' || ? || '%') ORDER BY RANDOM() LIMIT 1""", (rese_id,))
-    stmt = text("""SELECT so.title, so.similarity_percentage, pa.url, pa.title as titulo_alicia FROM "Similar_Orcid" so  INNER JOIN "Publicacion_Alicia" pa ON so.identifier=pa.identifier where so.link_dina like ('%'|| :x) ORDER BY RANDOM() LIMIT 1""")
-    query = conn.execute(stmt,x=rese_id)
-    result = {'recommendation': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-    return jsonify(result),{'Content-Type': 'application/json'} #{'Body': jsonify(result)},
+class Recommendations(Resource):
+    def get(self, dato):
+		host = "ec2-50-16-196-238.compute-1.amazonaws.com:5432"
+		db = "de925n6fc4qpge"
+		user = "aaiekzlymkmulx"
+		pw = "688099ba64af630040260c2d81a5354098b4773ca07fb0163d49ee36928fa342"
+        db_connect = create_engine('postgres://'+user+':'+pw+'@'+host+'/'+db)
+		rese_id ='https://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador='+str(dato)
+		s=text("(select a.title, t.similarity_percentage, a.link from alicia a, scopus_similar t where t.link_dina=:dato and a.identifier=t.identifier) union (select a.title, t.similarity_percentage, a.link from alicia a, orcid_similar t where t.link_dina=:dato and a.identifier=t.identifier) order by similarity_percentage desc limit 10;")
+		query=conn.execute( s, dato=rese_id)
+		result = {'recommendations': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+		conn.close()
+		db_connect.dispose()	
+        return jsonify(result)
+class RandomRecommendation(Resource):
+    def get(self, dato):
+        host = "ec2-50-16-196-238.compute-1.amazonaws.com:5432"
+		db = "de925n6fc4qpge"
+		user = "aaiekzlymkmulx"
+		pw = "688099ba64af630040260c2d81a5354098b4773ca07fb0163d49ee36928fa342"
+        db_connect = create_engine('postgres://'+user+':'+pw+'@'+host+'/'+db)
+		rese_id ='https://dina.concytec.gob.pe/appDirectorioCTI/VerDatosInvestigador.do?id_investigador='+str(dato)
+		s=text("(select a.title, t.similarity_percentage, a.link from alicia a, scopus_similar t where t.link_dina=:dato and a.identifier=t.identifier order by random()) union (select a.title, t.similarity_percentage, a.link from alicia a, orcid_similar t where t.link_dina=:dato and a.identifier=t.identifier order by random())  limit 1;")
+        result = {'recommendation': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        conn.close()
+		db_connect.dispose()
+		return jsonify(result)
+class Shutdown(Resource):
+    def get(self):
+        func = request.environ.get('werkzeug.server.shutdown')
+        if func is None:
+            raise RuntimeError('Not running with the Werkzeug Server')
+        func()
 
-@app.route('/shutdown')        
-def shutdown():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
+class Service:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.api = Api(self.app)
+        
+        #self.api.add_resource(Prueba, '/prueba') #'/employees/<employee_id>'
+        self.api.add_resource(Recommendations, '/recomendaciones/<dato>')
+        self.api.add_resource(RandomRecommendation, '/recomendacion_aleatoria/<dato>')
+        self.api.add_resource(Shutdown, '/shutdown')
 
+        
+    def run(self):
+        self.app.run(port=5002)
+        
+        
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
-
-
-
+    print("ANTES")
+    servicio=Service()
+    servicio.run()
+    print("HOLA")
